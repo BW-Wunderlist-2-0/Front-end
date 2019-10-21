bb323c8156ad4d74edf6e6ea98a40994bb7098b4
Aaron Clark <aaronpclark92@gmail.com>
2019-10-21 16:43:34 -0500

    Added Home component with toggling drawer

--- wunderlist-frontend/src/App.js
+++ wunderlist-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
+import Home from './components/Home';
 import PrivateRoute from './components/PrivateRoute';
 import Navigation from './components/Navigation';
 
@@ -13,6 +14,10 @@ function App() {
         <h1>Wunderlist 2.0</h1>
         <Navigation />
         <Switch>
+
+          {/* PrivateRoute temporarily disabled */}
+          {/* <PrivateRoute path='/home' component={Home} /> */}
+          <Route path='/home' component={Home} />
           <Route path='/login' component={Login} />
         </Switch>
       </div>
