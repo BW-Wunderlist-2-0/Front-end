5a7bb5a1814d1807da5c9774a1def996f7f6d615
Aaron Clark <aaronpclark92@gmail.com>
2019-10-21 14:52:38 -0500

    removed unused state on Login component

--- wunderlist-frontend/src/components/Login.js
+++ wunderlist-frontend/src/components/Login.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Form, Icon, Input, Button } from 'antd';
 
-const Login = () => {
+
+const initialLoginState = {
+  username: '',
+  password: ''
+}
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+const Login = props => {
+  const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = props.form
+
+
+  const usernameError = isFieldTouched('username') && getFieldError('username');
+  const passwordError = isFieldTouched('password') && getFieldError('password');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
 
   return (
     <div>
-      Login Component
+      <h1> Login Component</h1>
+      <Form layout="inline" onSubmit={handleSubmit}>
+        <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''}>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item validateStatus={passwordError ? 'error' : ''} help={passwordError || ''}>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   )
 }
 
-export default Login;
+const WrappedLogin = Form.create({ name: 'login' })(Login);
+
+
+export default WrappedLogin;
