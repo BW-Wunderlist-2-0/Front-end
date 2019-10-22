68b92ffeb0905ae476d3857adddf184382afc089
Aaron Clark <aaronpclark92@gmail.com>
2019-10-21 21:24:07 -0500

    Removed Router from Index.js

--- wunderlist-frontend/src/index.js
+++ wunderlist-frontend/src/index.js
@@ -11,15 +11,18 @@ import "./index.css";
 import App from "./App";
 import { reducer } from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))
+);
 
 /*store*/
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+
+    <App />
+
   </Provider>,
   document.getElementById("root")
 );
